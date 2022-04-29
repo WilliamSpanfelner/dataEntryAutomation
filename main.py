7b6431d2df9c82c98765927eff251bb4bf3af0ed
WilliamSpanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-04-29 13:04:57 +0300

    Add headers to read page

--- main.py
+++ main.py
@@ -17,8 +17,20 @@ ZILLOW_URL = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7
              "%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B" \
              "%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
 
+# Add headers to circumvent captcha throws as per Max Bade (Scraping Zillow with Python and BeautifulSoup)[
+# https://blog.devgenius.io/scraping-zillow-with-python-and-beautifulsoup-bbc7e581c218]
+headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.8',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+}
+
 # Read Zillow content
-response = requests.get(ZILLOW_URL)
+with requests.Session() as session:
+    response = session.get(ZILLOW_URL, headers=headers)
+
 zillow_page = response.text
 
 # Make soup
