dc7a214ece02e37042aebb662710cde5b257be37
WilliamSpanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-05-16 12:31:59 +0300

    Create functions to extract data

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import requests
 # - send to google form
 # and then to a spreadsheet
 
+ROOT_URL = 'https://www.zillow.com'
+
 FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdXvhrvVZ0-nBnYvSJdkrzCoCLJsNut-IO5xn-bDlm_5drWiA/viewform?usp" \
            "=sf_link "
 ZILLOW_URL = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C" \
@@ -16,6 +18,54 @@ ZILLOW_URL = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7
              "%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C" \
              "%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B" \
              "%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
+
+
+def extract_rent_from(bs4_rent):
+    """
+    Returns a string representing the monthly rent clear of
+    extraneous text
+    :param bs4_rent: the BeautifulSoup object containing the rent;
+    :return: the rent as a string.
+    """
+    rent = ''
+    price: str = bs4_rent[0].get_text()
+    if "/" in price:
+        price = price.split('/')[0]
+    elif "+" in price:
+        price = price.split('+')[0]
+    rent = price
+    return rent
+
+
+def extract_address_from(bs4_address, rent):
+    """
+    Returns a clean address string
+
+    :param bs4_address: a BeautifulSoup object containing the address;
+    :param rent: a string representing the rent;
+    :return: an address string.
+    """
+    address = bs4_address[0].get_text()
+    if rent in address:
+        # print(f"adrress must be split: {address.split(rent)}")
+        address = address.split(rent + ', ')[1]
+
+    return address
+
+
+
+def extract_url_from(bs4_url):
+    """
+    Returns a url as a string from a BeautifulSoup object
+    :param bs4_url: a BeautifulSoup object containing the url;
+    :return: a url as a string.
+    """
+    url = bs4_url[0].get('href')
+    if url[:2] == "/b":
+        url = ROOT_URL + url
+    return url
+
+
 
 # Add headers to circumvent captcha throws as per Max Bade (Scraping Zillow with Python and BeautifulSoup)[
 # https://blog.devgenius.io/scraping-zillow-with-python-and-beautifulsoup-bbc7e581c218]
@@ -35,7 +85,29 @@ zillow_page = response.text
 
 # Make soup
 soup = BeautifulSoup(zillow_page, "html.parser")
-print(soup)
 
-properties = soup.find_all(name="a", id="top")
-print("properties: ", properties)
+# Strain the property cards from the soup
+cards = soup.select("ul > li > article > div.list-card-info")
+
+# Create a list to hold property data
+properties = []
+
+# Extract data to the list
+for card in cards:
+    rent = ''
+    bs4_rent = card.select("div.list-card-price")
+    if bs4_rent:
+        rent = extract_rent_from(bs4_rent)
+
+    address = ''
+    bs4_address = card.select("address")
+    if bs4_address:
+        address = extract_address_from(bs4_address, rent)
+
+    url = ''
+    bs4_url = card.select("a.list-card-link")
+    if bs4_url:
+        url = extract_url_from(bs4_url)
+
+    if rent and address and url:
+        properties.append((rent, address, url))
